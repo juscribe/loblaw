fb53fa8eb6901a166bfbea5152a97a0180ea3f7a
caleon <caleon@gmail.com>
2013-03-09 13:43:51 -0800

    Trying to hide empty task from list of tasks

--- lib/generators/loblaw/install/install_generator.rb
+++ lib/generators/loblaw/install/install_generator.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
-require 'rails/generators/active_record'
+# require 'rails/generators/active_record'
 
 module Loblaw
   module Generators
-    class InstallGenerator < Rails::Generators::NamedBase
-      source_root File.expand_path('../templates', __FILE__)
-    end
+    # class InstallGenerator < Rails::Generators::NamedBase
+    #   source_root File.expand_path('../templates', __FILE__)
+    # end
   end
 end
